86fff1ade97f50f9663fd57d75dc8c62038dcc89
Aleksandr KUDRYAVETS <Aleksandr.KUDRYAVETS@raiffeisen.ru>
2024-09-09 19:11:19 +0300

    feat: annotation

---
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import exercise.model.Address;
+import exercise.annotation.Inspect;
+import java.lang.reflect.Method;
+
+public class Application {
+    public static void main(String[] args) {
+        var address = new Address("London", 12345678);
+
+        // BEGIN
+        for (Method method : Address.class.getDeclaredMethods()) {
+
+            // Проверяем, есть ли у метода аннотация @LogExecutionTime
+            if (method.isAnnotationPresent(Inspect.class)) {
+
+                var startTime = System.currentTimeMillis();
+
+                try {
+                    // Выполняем метод с аннотацией LogExecutionTime
+                    method.invoke(address);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                long endTime = System.currentTimeMillis();
+                long executionTime = endTime - startTime;
+
+                System.out.println("Method " + method.getName() + " returns a value of type " + method.getReturnType().getSimpleName());
+            }
+        }
+        // END
+    }
+}
