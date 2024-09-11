b518cc87ca2a58f7adc47e38b02aaea78f5fa28a
@hexlet/cli <support@hexlet.io>
2024-09-11 22:34:31 +0300

    submit java-spring-ru/search

---
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -0,0 +1,35 @@
+package exercise.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import exercise.dto.ProductParamsDTO;
+import exercise.model.Product;
+
+// BEGIN
+@Component
+public class ProductSpecification {
+    public Specification<Product> build(ProductParamsDTO params) {
+        return withCategoryId(params.getCategoryId())
+                .and(withTitleCont(params.getTitleCont()))
+                .and(withPriceGt(params.getPriceGt()))
+                .and(withPriceLt(params.getPriceLt()))
+                .and(withRating(params.getRatingGt()));
+    }
+    public Specification<Product> withCategoryId(Long categoryId) {
+        return ((root, query, criteriaBuilder) -> categoryId == null ? criteriaBuilder.conjunction() : criteriaBuilder.equal(root.get("category").get("id"), categoryId));
+    }
+    public Specification<Product> withTitleCont(String substring) {
+        return ((root, query, criteriaBuilder) -> substring == null ? criteriaBuilder.conjunction() : criteriaBuilder.like(root.get("title"), "%" + substring + "%"));
+    }
+    public Specification<Product> withPriceGt(Integer priceGt) {
+        return (root, query, criteriaBuilder) -> priceGt == null ? criteriaBuilder.conjunction() : criteriaBuilder.greaterThan(root.get("price"), priceGt);
+    }
+    public Specification<Product> withPriceLt(Integer priceLt) {
+        return ((root, query, criteriaBuilder) -> priceLt == null ? criteriaBuilder.conjunction() : criteriaBuilder.lessThan(root.get("price"), priceLt));
+    }
+    public Specification<Product> withRating(Double ratingGt) {
+        return ((root, query, criteriaBuilder) -> ratingGt == null ? criteriaBuilder.conjunction() : criteriaBuilder.greaterThan(root.get("rating"), ratingGt));
+    }
+}
+// END
